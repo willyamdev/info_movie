81ea0164e21b8f475cbd613a50474be3f6da9ec8
Willyam Biancardi <willyamvinicius1900@gmail.com>
2020-11-16 22:22:36 -0300

    added toolbar component

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import Home from './pages/home/Home';
 import './pages/global/css/global.css';
+import Toolbar from './pages/global/components/toolbar/Toolbar';
 
 export default function App() : JSX.Element{
   return (
     <div className="app">
+
+      <Toolbar/>
 
       <Route
         path="/"
