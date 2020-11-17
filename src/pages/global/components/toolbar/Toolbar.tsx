563b1fa29ede53ce270915f9dc0c4f84434afc71
Willyam Biancardi <willyamvinicius1900@gmail.com>
2020-11-16 22:20:11 -0300

    added toolbar

---
+++ src/pages/global/components/toolbar/Toolbar.tsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useRef } from 'react';
+import InfoMovie from '../info-movie-logo/InfoMovie';
+import './css/toolbar.css';
+import { Link } from 'react-router-dom';
+
+export default function Toolbar() {
+
+    let toolbarRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        window.onscroll = () => {
+            let toolbarElement = toolbarRef.current;
+            let currentScrollPos = window.pageYOffset;
+
+            if (currentScrollPos < 100) {
+                toolbarElement?.setAttribute('class', 'toolbar');
+            } else if (currentScrollPos == 0) {
+                toolbarElement?.setAttribute('class', 'toolbar');
+            } else {
+                toolbarElement?.setAttribute('class', 'toolbar toolbar-expanded');
+            }
+
+            //SE QUISER QUE A BARRA ENCOLHA SEMPRE QUE FOR PARA CIMA
+            // prevScrollPos = currentScrollPos;
+        }
+    }, []);
+
+    return (
+        <div className="toolbar" ref={toolbarRef}>
+            <Link to="/">
+                <InfoMovie fontSize={22} />
+            </Link>
+
+            <div className="toolbar-links">
+                <Link to="/">Movies</Link>
+                <Link to="/">Genres</Link>
+                <Link to="/">Articles</Link>
+                <Link to="/">About Us</Link>
+                <Link to="/">Account</Link>
+            </div>
+        </div>
+    );
+
+}
